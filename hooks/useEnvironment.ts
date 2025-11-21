12c0ee4f4bd4630c337cf0052816bc2c086ec713
Augustin Briolon <augustin.briolon@gmail.com>
2025-11-21 10:31:35 +0100

    try to remove performance indicator in Prod

--- hooks/useEnvironment.ts
+++ hooks/useEnvironment.ts
@@ -1,24 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useEnvironment = () => {
-  const getIsLocalhost = () => {
-    if (typeof window === 'undefined') return false;
-    return window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-  };
+  const [isProd, setIsProd] = useState(true);
+  const [isDev, setIsDev] = useState(true);
+  const [environment, setEnvironment] = useState('production');
 
-  const getIsProd = () => {
-    if (typeof window === 'undefined') return true;
-    return window.location.hostname.includes('crystal-vision.paranthese.studio');
-  };
+  useEffect(() => {
+    const isLocalhost =
+      window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    const isProd = window.location.hostname.includes('crystal-vision.paranthese.studio');
 
-  const getEnvironment = () => {
-    const isLocalhost = getIsLocalhost();
-    return isLocalhost ? 'development' : 'production';
-  };
-
-  const [isProd] = useState(() => getIsProd());
-  const [isDev] = useState(() => getIsLocalhost());
-  const [environment] = useState(() => getEnvironment());
+    setIsDev(isLocalhost);
+    setIsProd(isProd);
+    setEnvironment(isLocalhost ? 'development' : 'production');
+  }, []);
 
   return {
     isProd,
